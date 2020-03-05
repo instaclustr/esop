bbb5c18cdb4b279a49901435df87606014ed753e
Štefan Miklošovič <stefan.miklosovic@instaclustr.com>
2020-03-05 11:59:15 +0100

    added documentation and tests around storage location (#12)

--- src/test/java/com/instaclustr/cassandra/backup/StorageLocationTest.java
+++ src/test/java/com/instaclustr/cassandra/backup/StorageLocationTest.java
@@ -2,6 +2,8 @@ package com.instaclustr.cassandra.backup;
 
 import static org.testng.Assert.assertEquals;
 
+import java.nio.file.Paths;
+
 import com.instaclustr.cassandra.backup.impl.StorageLocation;
 import org.testng.annotations.Test;
 
@@ -11,15 +13,20 @@ public class StorageLocationTest {
     public void storageLocationTest() {
         StorageLocation storageLocation = new StorageLocation("gcp://bucket/cluster/dc/node");
 
+        storageLocation.validate();
+
         assertEquals(storageLocation.storageProvider, "gcp");
         assertEquals(storageLocation.bucket, "bucket");
         assertEquals(storageLocation.clusterId, "cluster");
         assertEquals(storageLocation.datacenterId, "dc");
         assertEquals(storageLocation.nodeId, "node");
+    }
 
-        storageLocation.validate();
-
+    @Test
+    public void fileLocationTest() {
         StorageLocation fileLocation = new StorageLocation("file:///some/path/bucket/cluster/dc/node");
+
+        fileLocation.validate();
 
         assertEquals(fileLocation.storageProvider, "file");
         assertEquals(fileLocation.fileBackupDirectory.toString(), "/some/path");
@@ -27,7 +34,33 @@ public class StorageLocationTest {
         assertEquals(fileLocation.clusterId, "cluster");
         assertEquals(fileLocation.datacenterId, "dc");
         assertEquals(fileLocation.nodeId, "node");
+    }
+
+    @Test
+    public void fileLocationTest2() {
+        StorageLocation fileLocation = new StorageLocation("file:///tmp/a/b/c/d/");
 
         fileLocation.validate();
+
+        assertEquals(fileLocation.storageProvider, "file");
+        assertEquals(fileLocation.fileBackupDirectory.toString(), "/tmp");
+        assertEquals(fileLocation.bucket, "a");
+        assertEquals(fileLocation.clusterId, "b");
+        assertEquals(fileLocation.datacenterId, "c");
+        assertEquals(fileLocation.nodeId, "d");
+    }
+
+    @Test
+    public void fileLocationTest3() {
+        StorageLocation fileLocation = new StorageLocation("file:///a/b/c/d/");
+
+        fileLocation.validate();
+
+        assertEquals(fileLocation.storageProvider, "file");
+        assertEquals(fileLocation.fileBackupDirectory.toString(), Paths.get("").toAbsolutePath().toString());
+        assertEquals(fileLocation.bucket, "a");
+        assertEquals(fileLocation.clusterId, "b");
+        assertEquals(fileLocation.datacenterId, "c");
+        assertEquals(fileLocation.nodeId, "d");
     }
 }
