5f89acbd11a2c7c2b5dcfcf008732d4d5fc31900
Chamith Edirisinghe <chamithedirisinghe@Chamiths-MBP.lan>
2021-10-15 17:32:00 +1100

    azure and gcp listing and removal

--- src/main/java/com/instaclustr/esop/gcp/GCPRestorer.java
+++ src/main/java/com/instaclustr/esop/gcp/GCPRestorer.java
@@ -1,5 +1,6 @@
 package com.instaclustr.esop.gcp;
 
+import static com.instaclustr.esop.impl.list.ListOperationRequest.getForLocalListing;
 import static java.lang.String.format;
 import static java.util.stream.Collectors.toList;
 
@@ -14,7 +15,9 @@ import java.util.List;
 import java.util.function.Consumer;
 import java.util.function.Predicate;
 import java.util.regex.Pattern;
+import java.util.stream.StreamSupport;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.api.gax.paging.Page;
 import com.google.cloud.ReadChannel;
 import com.google.cloud.storage.Blob;
@@ -28,15 +31,22 @@ import com.google.inject.assistedinject.AssistedInject;
 import com.instaclustr.esop.gcp.GCPModule.GoogleStorageFactory;
 import com.instaclustr.esop.impl.Manifest;
 import com.instaclustr.esop.impl.RemoteObjectReference;
+import com.instaclustr.esop.impl.StorageLocation;
 import com.instaclustr.esop.impl.list.ListOperationRequest;
 import com.instaclustr.esop.impl.remove.RemoveBackupRequest;
 import com.instaclustr.esop.impl.restore.RestoreCommitLogsOperationRequest;
 import com.instaclustr.esop.impl.restore.RestoreOperationRequest;
 import com.instaclustr.esop.impl.restore.Restorer;
+import com.instaclustr.esop.local.LocalFileRestorer;
+import com.instaclustr.io.FileUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class GCPRestorer extends Restorer {
+    private static final Logger logger = LoggerFactory.getLogger(GCPRestorer.class);
 
     private final Storage storage;
+    LocalFileRestorer localFileRestorer;
 
     @AssistedInject
     public GCPRestorer(final GoogleStorageFactory storageFactory,
@@ -54,16 +64,24 @@ public class GCPRestorer extends Restorer {
 
     @AssistedInject
     public GCPRestorer(final GoogleStorageFactory storageFactory,
-                       @Assisted final ListOperationRequest request) {
+                       @Assisted final ListOperationRequest request,
+                       final ObjectMapper objectMapper) {
         super(request);
         this.storage = storageFactory.build(request);
+
+        this.localFileRestorer = new LocalFileRestorer(getForLocalListing(request, request.cacheDir, request.storageLocation),
+                objectMapper);
     }
 
     @AssistedInject
     public GCPRestorer(final GoogleStorageFactory storageFactory,
-                       @Assisted final RemoveBackupRequest request) {
+                       @Assisted final RemoveBackupRequest request,
+                       ObjectMapper objectMapper) {
         super(request);
         this.storage = storageFactory.build(request);
+
+        this.localFileRestorer = new LocalFileRestorer(getForLocalListing(request, request.cacheDir, request.storageLocation),
+                objectMapper);
     }
 
     @Override
@@ -174,6 +192,13 @@ public class GCPRestorer extends Restorer {
         return blobItems.get(0).getName();
     }
 
+    private List<String> getBlobPaths(final Page<Blob> blobs, final Predicate<String> keyFilter) {
+        return StreamSupport.stream(blobs.iterateAll().spliterator(), false)
+                            .map(BlobInfo::getName)
+                            .filter(keyFilter)
+                            .collect(toList());
+    }
+
     @Override
     public void consumeFiles(final RemoteObjectReference prefix, final Consumer<RemoteObjectReference> consumer) {
         final GCPRemoteObjectReference gcpRemoteObjectReference = (GCPRemoteObjectReference) prefix;
@@ -186,6 +211,90 @@ public class GCPRestorer extends Restorer {
                 consumer.accept(objectKeyToNodeAwareRemoteReference(removeNodePrefix(blob)));
             }
         });
+    }
+
+    public void downloadManifestsToDirectory(Path downloadDir) throws Exception {
+        FileUtils.createDirectory(downloadDir);
+        FileUtils.cleanDirectory(downloadDir.toFile());
+        final List<String> manifestKeys = getBlobPaths(storage.list(storageLocation.bucket),
+                s -> s.contains("manifests"));
+        for (String o: manifestKeys) {
+            Path manifestPath = Paths.get(o);
+            Path manifestName = manifestPath.getFileName();
+
+            Path destination = downloadDir.resolve(getStorageLocation().nodePath())
+                    .resolve("manifests")
+                    .resolve(manifestName);
+
+            downloadFile(destination, objectKeyToRemoteReference(manifestPath));
+        }
+    }
+
+    @Override
+    public List<Manifest> listManifests() throws Exception {
+        //If skipDownload flag is not set, download manifests
+        if (this.request instanceof ListOperationRequest) {
+            if (!((ListOperationRequest) this.request).skipDownload) {
+                StorageLocation location = this.localFileRestorer.getStorageLocation();
+                Path downloadDirectory = location.fileBackupDirectory.resolve(this.localFileRestorer.getStorageLocation().bucket);
+                downloadManifestsToDirectory(downloadDirectory);
+            }
+        }
+        return localFileRestorer.listManifests();
+    }
+
+    @Override
+    public void delete(final Path objectKey) throws Exception {
+        final RemoteObjectReference remoteObjectReference = objectKeyToNodeAwareRemoteReference(objectKey);
+        final Path fileToDelete = Paths.get(request.storageLocation.bucket,
+                remoteObjectReference.canonicalPath);
+        logger.info("Non dry: " + fileToDelete);
+        storage.delete(((GCPRemoteObjectReference) remoteObjectReference).blobId);
+    }
+
+    @Override
+    public void delete(final Manifest.ManifestReporter.ManifestReport backupToDelete, final RemoveBackupRequest request) throws Exception {
+        logger.info("Deleting backup {}", backupToDelete.name);
+        if (backupToDelete.reclaimableSpace > 0 && !backupToDelete.getRemovableEntries().isEmpty()) {
+            for (final String removableEntry : backupToDelete.getRemovableEntries()) {
+                if (!request.dry) {
+                    delete(Paths.get(removableEntry));
+                } else {
+                    logger.info("Dry: " + removableEntry);
+                }
+            }
+        }
+
+        // manifest and topology as the last
+        if (!request.dry) {
+            //delete in GCP
+            delete(backupToDelete.manifest.objectKey);
+            //delete in local cache
+            localFileRestorer.delete(backupToDelete.manifest.objectKey);
+        } else {
+            logger.info("Dry: " + backupToDelete.manifest.objectKey);
+        }
+
+    }
+
+    @Override
+    public List<StorageLocation> listNodes() throws Exception {
+        return localFileRestorer.listNodes();
+    }
+
+    @Override
+    public List<StorageLocation> listNodes(final String dc) throws Exception {
+        return localFileRestorer.listNodes(dc);
+    }
+
+    @Override
+    public List<StorageLocation> listNodes(final List<String> dcs) throws Exception {
+        return localFileRestorer.listNodes(dcs);
+    }
+
+    @Override
+    public List<String> listDcs() throws Exception {
+        return localFileRestorer.listDcs();
     }
 
     private Path removeNodePrefix(final Blob blob) {
