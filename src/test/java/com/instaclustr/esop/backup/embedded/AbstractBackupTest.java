cf0bc65fa1427b5bf34e8dcb5fbb7e9cd0cd9a53
Stefan Miklosovic <smiklosovic@apache.org>
2022-06-28 23:17:53 +0200

    updated versions of c* to 3.11.13 and 4.1-alpha1

--- src/test/java/com/instaclustr/esop/backup/embedded/AbstractBackupTest.java
+++ src/test/java/com/instaclustr/esop/backup/embedded/AbstractBackupTest.java
@@ -89,9 +89,9 @@ public abstract class AbstractBackupTest {
 
     protected static final Logger logger = LoggerFactory.getLogger(AbstractBackupTest.class);
 
-    public static final String CASSANDRA_VERSION = System.getProperty("cassandra3.version", "3.11.10");
+    public static final String CASSANDRA_VERSION = System.getProperty("cassandra3.version", "3.11.13");
 
-    public static final String CASSANDRA_4_VERSION = System.getProperty("cassandra4.version", "4.0.3");
+    public static final String CASSANDRA_4_VERSION = System.getProperty("cassandra4.version", "4.1-alpha1");
 
     // This is number of rows we inserted into Cassandra DB in total
     // we backed up first 6 rows. For the last two rows, they are stored in commit logs.
