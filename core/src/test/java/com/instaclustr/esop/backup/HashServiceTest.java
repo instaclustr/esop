b64bf48e4b82f120593bd1114b6ae240fdeca33b
Bohdan Siryk <bohdan.siryk@netapp.com>
2025-10-22 12:03:17 +0300

    Added all supported algorithms to HashServiceTest

--- core/src/test/java/com/instaclustr/esop/backup/HashServiceTest.java
+++ core/src/test/java/com/instaclustr/esop/backup/HashServiceTest.java
@@ -6,16 +6,54 @@ import java.nio.file.Files;
 import com.instaclustr.esop.impl.hash.HashService;
 import com.instaclustr.esop.impl.hash.HashServiceImpl;
 import com.instaclustr.esop.impl.hash.HashSpec;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 public class HashServiceTest {
 
-    @Test
-    public void testHashing() throws Exception {
-        final File f = File.createTempFile("hashingTest", ".tmp");
-        Files.write(f.toPath(), "".getBytes());
-        final HashService hashService = new HashServiceImpl(new HashSpec());
-        hashService.verify(f.toPath(), hashService.hash(f.toPath()));
+    private static File testFile;
+
+    @BeforeAll
+    public static void setup() throws Exception {
+        testFile = File.createTempFile("hashingTest", ".tmp");
+        Files.write(testFile.toPath(), "testdata".getBytes());
     }
 
+    @AfterAll
+    public static void teardown() throws Exception {
+        if (testFile != null && testFile.exists()) {
+            testFile.delete();
+        }
+    }
+
+    @Test
+    public void testHashing_DefaultAlgorithm() throws Exception {
+        testHashing(new HashSpec());
+    }
+
+    @Test
+    public void testHashing_SHA256() throws Exception {
+        testHashing(new HashSpec(HashSpec.HashAlgorithm.SHA_256));
+    }
+
+    @Test
+    public void testHashing_CRC32() throws Exception {
+        testHashing(new HashSpec(HashSpec.HashAlgorithm.CRC));
+    }
+
+    @Test
+    public void testHashing_xxHash64() throws Exception {
+        testHashing(new HashSpec(HashSpec.HashAlgorithm.XXHASH64));
+    }
+
+    @Test
+    public void testHashing_None() throws Exception {
+        testHashing(new HashSpec(HashSpec.HashAlgorithm.NONE));
+    }
+
+    private void testHashing(HashSpec hashSpec) throws Exception {
+        final HashService hashService = new HashServiceImpl(hashSpec);
+        hashService.verify(testFile.toPath(), hashService.hash(testFile.toPath()));
+    }
 }
